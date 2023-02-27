5c7f69af506a24b46afd0eaba5fec2d465489a69
im-saurabh <saurabhmhase79@gmail.com>
2023-02-27 22:16:07 +0530

    first commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import html from "../assets/html.png";
 import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
-import bootstrap from "../assets/bootstrap.png";
+import bootstrap from "../assets/Bootstrap.png";
 import mongodb from "../assets/mongodb.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
