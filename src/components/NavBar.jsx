a3ad0f5932753037d2dd947e5fca2dd47421a495
Saurabh Mhase <63945720+im-saurabh@users.noreply.github.com>
2025-07-13 20:30:43 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-gradient-to-r from-black fixed">
       <div>
         <h1 className="text-4xl sm:text-5xl font-signature font-bold ml-2">
-          Saurabh Mhase
+          
         </h1>
       </div>
 
